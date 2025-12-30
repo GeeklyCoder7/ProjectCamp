dd0327298291e2dd26a17a6cf3a53f38a1ed895e
GeeklyCoder7 <afwanhusaini07@gmail.com>
2025-12-30 17:13:29 +0530

    feat(auth): implement verifyJWT middleware to protect authenticated routes

---
+++ src/middlewares/auth.middlewares.js
@@ -0,0 +1,36 @@
+import { User } from "../models/user.models.js";
+import { ApiError } from "../utils/api-error.js";
+import { asyncHandler } from "../utils/async-handler.js";
+import jwt from "jsonwebtoken";
+
+const verifyJWT = asyncHandler(async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  // Checking if the header is present
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new ApiError(401, "Unauthorized request: Token missing");
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  // Decoding and verifying the token
+  let decodedToken;
+  try {
+    decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+  } catch (error) {
+    throw new ApiError(401, "Invalid or expires JWT token.");
+  }
+
+  // Checking if any user with the token exist
+  const user = await User.findOne({_id: decodedToken._id}).select("-password -refreshToken");
+
+  if (!user) {
+    throw new ApiError(401, "User not found");
+  }
+
+  req.user = user;
+
+  next();
+});
+
+export { verifyJWT };
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,21 @@
 import { Router } from "express";
-import { login, registerUser, verifyEmail } from "../controllers/auth.controllers.js";
+import {
+  login,
+  registerUser,
+  verifyEmail,
+} from "../controllers/auth.controllers.js";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const authRouter = Router();
 authRouter.post("/register", registerUser); // Used for /users/register
 authRouter.post("/login", login); // Used for /users/register
 authRouter.post("/verify-email/:token", verifyEmail); // Used for /users/verify-email/:token
+
+authRouter.get("/me", verifyJWT, (req, res) => {
+  return res.status(200).json({
+    message: "You are authorized! Access granted!",
+    data: req.user,
+  });
+});
 
 export default authRouter;
